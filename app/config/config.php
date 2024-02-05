354643a3385c8237eaf7fa18a620bfdf497d9708
ThePope05 <swobben05@gmail.com>
2024-02-05 11:22:17 +0100

    Added database script

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', '');
+define('DB_PASS', '15122005');
+define('DB_NAME', 'snoepwinkel');
 define('URLROOT', 'http://localhost:');
 define('PORT', '8000');//ONLY WORKS FOR LOCALHOST
--- app/db/01_Create_Table_Example.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS `example` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
