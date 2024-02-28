27a976319ab5d994f144ec45d96a4f7d96edd5a9
ThePope05 <swobben05@gmail.com>
2024-02-28 21:45:03 +0100

    Add ButtonLink component

---
+++ app/views/Components/ButtonLink.php
@@ -0,0 +1,3 @@
+<a href="<?= $componentData['route'] ?>" class="px-6 py-2 bg-gradient-to-tr w-max mx-auto from-orange-500 to-red-800 hover:from-orange-400 hover:to-red-800 text-slate-800 font-bold rounded">
+    <?= $componentData['text'] ?>
+</a>
